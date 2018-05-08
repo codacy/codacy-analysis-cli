5b0029c6574fc5e190ce25a0dfb37f6d34e8c877
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2018-05-08 11:15:53 +0100

    Improve matching with explicit Left

--- src/main/scala/com/codacy/analysis/cli/upload/ResultsUploader.scala
+++ src/main/scala/com/codacy/analysis/cli/upload/ResultsUploader.scala
@@ -22,7 +22,7 @@ class ResultsUploader(commitUuid: String, codacyClient: CodacyClient, batchSizeO
   def sendResults(tool: String, results: Set[Result]): Future[Either[String, Unit]] = {
     uploadResultsBatch(tool, batchSize, results).flatMap {
       case Right(_) => endUpload()
-      case x        => Future.successful(x)
+      case error @ Left(_) => Future.successful(error)
     }
   }
 
