c9fe50106d3969357a0deedc01ef77211030ac99
Bruno Ferreira <bruno.ferreira@codacy.com>
2018-06-20 17:33:28 +0100

    Add cli core publish as a library

---
+++ scripts/publish_lib.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+CURRENT_BRANCH="$(git symbolic-ref --short HEAD)"
+PUBLISH_BRANCH="master"
+
+BASE_VERSION="0.1.0-pre"
+DEFAULT_VERSION="${BASE_VERSION}.${CURRENT_BRANCH}-SNAPSHOT"
+
+if [ -n "$1" ]; then
+  VERSION="$1"
+else
+  VERSION="$DEFAULT_VERSION"
+fi
+
+echo "Publishing version ${VERSION}"
+if [[ -n "$CI" ]] && [[ "$CURRENT_BRANCH" == "$PUBLISH_BRANCH" || "$CIRCLE_BRANCH" == "$PUBLISH_BRANCH" ]]; then
+  sbt 'set version := "'"${VERSION}"'"' 'set pgpPassphrase := Some("'"$SONATYPE_GPG_PASSPHRASE"'".toCharArray)' publishSigned
+  sbt 'set version := "'"${VERSION}"'"' sonatypeRelease
+else
+  sbt 'set version := "'"${VERSION}"'"' publishLocal
+fi
