915437263fd49f3cbc933e875c176f41ad6bda75
Hugo Sousa <hugo@codacy.com>
2018-10-02 18:02:42 +0100

    Codacy compliance

--- core/src/main/scala/com/codacy/analysis/core/utils/IOHelper.scala
+++ core/src/main/scala/com/codacy/analysis/core/utils/IOHelper.scala
@@ -3,6 +3,8 @@ package com.codacy.analysis.core.utils
 import cats.MonadError
 import scalaz.zio.IO
 
+import scala.util.{Failure, Success, Try}
+
 object IOHelper {
   type IOThrowable[A] = IO[Throwable, A]
 
@@ -27,5 +29,11 @@ object IOHelper {
           IO.point(b)
       }
     }
+  }
+
+  implicit class IOOps[E, A](io: IO[E, A]) {
+
+    def redeemTry(implicit ev: E =:= Throwable): IO[Nothing, Try[A]] =
+      io.redeem(err => IO.point(Failure(err)), results => IO.point(Success(results)))
   }
 }
