2bf8d0b06f31d62acf31aa712c70cb89d3cbb35f
Bruno Ferreira <bmibferreira@gmail.com>
2018-12-28 20:14:53 +0000

    Update codacy-plugins dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -24,7 +24,7 @@ object Dependencies {
 
   lazy val caseApp = "com.github.alexarchambault" %% "case-app" % "1.2.0"
 
-  lazy val codacyPlugins = Seq("codacy" %% "codacy-plugins" % "5.0.339")
+  lazy val codacyPlugins = Seq("codacy" %% "codacy-plugins" % "5.0.343")
 
   lazy val fansi = "com.lihaoyi" %% "fansi" % "0.2.5"
 
