558f3f757508c534ffedb211a5bfda438f65634a
Paulo Martins <paulo.pontes.m@gmail.com>
2019-04-05 11:32:43 +0100

    fix: circleci

--- scripts/setup-aws-credentials.sh
+++ scripts/setup-aws-credentials.sh
@@ -10,5 +10,5 @@ aws_access_key_id=$ACCESS_KEY_ID
 aws_secret_access_key=$SECRET_ACCESS_KEY
 [shared-services]
 source_profile = default
-role_arn = arn:aws:iam::$ACCOUNT_ID:role/CredentialsBucketReader
+role_arn = arn:aws:iam::$AWS_ACCOUNT_ID_SHARED_SERVICES:role/CredentialsBucketReader
 EOF
