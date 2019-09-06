b99574860e00808017c5b6e5b1a190ca7246bd08
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2019-09-06 19:44:11 +0100

    bump: Bump codacy-plugins to 7.9.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  val codacyPluginsVersion = "7.9.2_play_2.6"
+  val codacyPluginsVersion = "7.9.3_play_2.6"
   val circeVersion = "0.9.3"
   val specs2Version = "4.0.2"
 
