6be1c48a0c351b55a9bf38a386ba9cfd6c0fd104
Scala Steward <me@scala-steward.org>
2020-05-12 16:21:30 +0200

    Update codacy-plugins, ... to 15.1.13_play_2.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  val codacyPluginsVersion = "15.1.12_play_2.7"
+  val codacyPluginsVersion = "15.1.13_play_2.7"
   val circeVersion = "0.12.3"
   val specs2Version = "4.8.1"
 
