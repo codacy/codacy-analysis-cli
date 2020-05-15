e937d3fdd530fcd0f69568eebc43ea780694bbfa
Scala Steward <me@scala-steward.org>
2020-05-15 18:19:20 +0200

    Update better-files to 3.9.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
     "io.circe" %% "circe-parser" % circeVersion,
     "io.circe" %% "circe-yaml" % "0.13.1")
 
-  lazy val betterFiles = "com.github.pathikrit" %% "better-files" % "3.6.0"
+  lazy val betterFiles = "com.github.pathikrit" %% "better-files" % "3.9.0"
 
   lazy val jodaTime = "joda-time" % "joda-time" % "2.10.6"
 
