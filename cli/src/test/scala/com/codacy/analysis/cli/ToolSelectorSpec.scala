567fc10cffc766487678c31e353bf0a3819082f9
Francisco <francisco@codacy.com>
2020-10-02 17:42:57 +0100

    fix: Fix some tests in ToolSelectorSpec

--- cli/src/test/scala/com/codacy/analysis/cli/ToolSelectorSpec.scala
+++ cli/src/test/scala/com/codacy/analysis/cli/ToolSelectorSpec.scala
@@ -4,7 +4,8 @@ import better.files.File
 import com.codacy.analysis.cli.analysis.ToolSelector
 import com.codacy.analysis.cli.configuration.CLIConfiguration
 import com.codacy.analysis.core.files.FilesTarget
-import com.codacy.analysis.core.tools.Tool
+import com.codacy.analysis.core.model.{AnalyserError, PatternSpec, ToolSpec}
+import com.codacy.analysis.core.tools.{Tool, ToolRepository}
 import com.codacy.analysis.core.utils.LanguagesHelper
 import com.codacy.plugins.api.languages.Languages.{Javascript, Python}
 import com.codacy.plugins.api.languages.{Language, Languages}
@@ -16,21 +17,52 @@ class ToolSelectorSpec extends Specification with NoLanguageFeatures {
   val emptyFilesTarget = FilesTarget(File(""), Set.empty, Set.empty)
   val noLocalConfiguration = Left("no config")
 
-  //TODO: Check what things we need to mock
-  val toolSelector: ToolSelector = new ToolSelector(toolRepository = null)
+  val toolRepository = new ToolRepository {
+
+    private def getToolSpec(uuid: String, name: String, languages: Set[Language]) =
+      ToolSpec(
+        uuid = uuid,
+        dockerImage = "codacy/codacy-example-tool:1.0.0",
+        isDefault = true,
+        version = "",
+        languages = languages,
+        name = name,
+        shortName = name,
+        documentationUrl = None,
+        sourceCodeUrl = None,
+        prefix = "",
+        needsCompilation = false,
+        hasConfigFile = true,
+        configFilenames = Set.empty,
+        isClientSide = false,
+        hasUIConfiguration = true)
+
+    override def list(): Either[AnalyserError, Seq[ToolSpec]] =
+      Right(
+        Seq(
+          getToolSpec("34225275-f79e-4b85-8126-c7512c987c0d", "PyLint", Set(Python)),
+          getToolSpec("c6273c22-5248-11e5-885d-feff819cdc9f", "Brakeman", Set(Languages.Ruby)),
+          getToolSpec("724f98da-f616-4e37-9606-f16919137a1e", "Rubocop", Set(Languages.Ruby)),
+          getToolSpec("38794ba2-94d8-4178-ab99-1f5c1d12760c", "BundlerAudit", Set(Languages.Ruby))))
+
+    override def get(uuid: String): Either[AnalyserError, ToolSpec] = ???
+
+    override def listPatterns(toolUuid: String): Either[AnalyserError, Seq[PatternSpec]] = Right(Seq.empty)
+  }
+  val toolSelector: ToolSelector = new ToolSelector(toolRepository)
 
   "AnalyseExecutor.allTools" should {
     "find python tools" in {
 
       val toolConfiguration =
         CLIConfiguration.Tool(Option.empty, allowNetwork = false, Left("no config"), Option.empty, Map.empty)
-      val pythonTools = toolSelector.allTools(None, toolConfiguration, Set(Languages.Ruby))
+      val rubyTools = toolSelector.allTools(None, toolConfiguration, Set(Languages.Ruby))
 
-      pythonTools should beRight
-      pythonTools must beLike {
+      rubyTools should beRight
+      rubyTools must beLike {
         case Right(tools) =>
           tools.map(_.name) must containTheSameElementsAs(
-            Seq("brakeman", "rubocop", "bundleraudit", "metrics", "duplication"))
+            Seq("Brakeman", "Rubocop", "Bundleraudit", "metrics", "duplication"))
           tools.flatMap(_.supportedLanguages) must containAllOf(Seq(Languages.Ruby))
       }
     }
@@ -39,7 +71,7 @@ class ToolSelectorSpec extends Specification with NoLanguageFeatures {
   "AnalyseExecutor.tools" should {
     "use input over remote configuration" in {
 
-      val expectedToolName = "pylint"
+      val expectedToolName = "PyLint"
       val toolConfigs =
         Set(CLIConfiguration.IssuesTool("InvalidToolName", enabled = true, notEdited = false, Set.empty))
       val toolConfiguration =
