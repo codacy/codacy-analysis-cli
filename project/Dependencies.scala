d931bc074c369eacc5530dd7a38a210b359d8e78
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2021-03-10 08:51:14 +0100

    bump: Bump codacy-plugins to 22.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  val codacyPluginsVersion = "21.7.0_play_2.7"
+  val codacyPluginsVersion = "22.3.0_play_2.7"
   val circeVersion = "0.12.3"
   val specs2Version = "4.8.1"
   val codacyApiVersion = "20.31.0"
